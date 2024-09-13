f9ea5a28aaba5f0a5194d0da3b066498f95c1071
Fahad Hassan <fahadhassan0001@gmail.com>
2024-09-14 03:26:24 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+var age = Number(prompt("Enter the age"));
+
+if(age <= 12){
+   alert ("You are classified as Child")
+}
+else if(age <=19){
+alert(" You are classified as Teenager")
+}
+else if(age <= 64){
+    alert(" You are classified as an Adult")
+    }
+    else {
+        alert(" You are classified as a senior")
+        }
+
